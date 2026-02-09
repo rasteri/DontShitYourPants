5e77aaac7206ffc541a867a52e942ac72e511768
rasteri <rasteri@gmail.com>
2026-02-09 02:34:18 +0000

    clean

--- noname.mk
+++
@@ -1,3 +0,0 @@
-project : C:\code\DontShitYourPants\noname.exe .SYMBOLIC
-
-!include C:\code\DontShitYourPants\noname.mk1
--- shitpants.mk
+++
@@ -1,3 +0,0 @@
-project : C:\code\DontShitYourPants\shitpants.exe .SYMBOLIC
-
-!include C:\code\DontShitYourPants\shitpants.mk1
